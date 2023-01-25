0c2c0640727833d3e6fbe497c51c3b37ac8b203e
Arseniy Kapkov <a.kapkov@digital-core.ru>
2023-01-25 22:18:16 +0300

    changes

---
+++ MyModel2.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+"""
+Network Architectures
+The following are the discriminator and generator architectures
+"""
+
+
+class discriminator(nn.Module):
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.fc1 = nn.Linear(10000, 512)
+        self.fc2 = nn.Linear(512, 1)
+        self.activation = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        x = x.view(-1, 10000)
+        x = self.activation(self.fc1(x))
+        x = self.fc2(x)
+        return nn.Sigmoid()(x)
+
+
+class generator(nn.Module):
+    def __init__(self):
+        super(generator, self).__init__()
+        self.fc1 = nn.Linear(128, 1024)
+        self.fc2 = nn.Linear(1024, 2048)
+        self.fc3 = nn.Linear(2048, 10000)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.fc3(x)
+        x = x.view(-1, 1, 10, 10)
+        return nn.Tanh()(x)
